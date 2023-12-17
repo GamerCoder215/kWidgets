113ff7ff44b041f75331399a4dbf35b4d8a94ac0
Gregory Mitchell <54124162+GamerCoder215@users.noreply.github.com>
2023-12-17 16:53:08 -0600

    Add New Provider Fields

--- core/src/main/kotlin/me/gamercoder215/kwidgets/Provider.kt
+++ core/src/main/kotlin/me/gamercoder215/kwidgets/Provider.kt
@@ -4,6 +4,8 @@ import java.util.*
 
 interface Provider {
 
+    // Fields
+
     val locale: Locale
 
     val fps: Int
@@ -16,7 +18,14 @@ interface Provider {
 
     val z: Int
 
+    val version: String
+
+    // Functions
+
     fun draw(text: String, x: Int, y: Int, size: Int, color: Int, shadow: Boolean = true)
+        = draw(text, x, y, size.toFloat(), color, shadow)
+
+    fun draw(text: String, x: Int, y: Int, size: Float, color: Int, shadow: Boolean = true)
 
     companion object {
         lateinit var provider: Provider
