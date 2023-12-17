1a2afccf98b895b434311599d13dded090a44dcd
Gregory Mitchell <54124162+GamerCoder215@users.noreply.github.com>
2023-12-17 16:53:11 -0600

    Update constants.kt

--- core/src/main/kotlin/me/gamercoder215/kwidgets/util/constants.kt
+++ core/src/main/kotlin/me/gamercoder215/kwidgets/util/constants.kt
@@ -2,6 +2,11 @@ package me.gamercoder215.kwidgets.util
 
 import java.awt.Color
 
+const val MOD_ID = "kwidgets"
+const val DEFAULT_FONT_SIZE = 11f
+
+// Maps
+
 val colors: Map<Color, String> =
     Color::class.java.declaredFields.filter { it.constant && it.type == Color::class.java && it.name[0].isUpperCase() }
         .associate { (it.get(null) as Color) to it.name }
